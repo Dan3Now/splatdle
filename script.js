cbb000039e0a76bef97f281d3ac0fd237f7851b7
Dan3Now <s3perdani@gmail.com>
2025-03-20 02:52:38 +0100

    implement partially guess select

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-//const response = await fetch("../assets/character/0_character.json");
-//var json_data = await response.json();
-//json_data = json_data["fr"];
-//console.log(json_data);
+//coding by Dan3_Now
+//coding in UTF-8
 
 class Game {
     constructor(list) {
         this.list = list;
 
-        this.game_input = document.getElementById("game_input");
+        this.characterList = [];
+
+        this.guess_input = document.getElementById("guess_input");
+        this.guess_select = document.getElementById("guess_select");
         this.guess_header = document.getElementById("guess_header");
         this.guess_lines = document.getElementById("guess_lines");
     }
@@ -20,20 +21,37 @@ class Game {
         //complete guess header
         for (let i=0; i<Object.keys(this.characterList[0]).length; i++) {
             let columnName = Object.keys(this.characterList[0])[i];
-
             let newDiv = document.createElement("div");
             let newContent = document.createTextNode(columnName);
             newDiv.appendChild(newContent);
             this.guess_header.append(newDiv);
         }
 
-        //complete select options
-        for (let i=0; i<this.characterList.length; i++) {
-            let opt = document.createElement("option");
+        //add event
+        this.guess_input.addEventListener("change", e => {
+            let possible_chr = this.characterList.filter(chr => {
+                return chr["name"].toLowerCase().includes(e.target.value);
+            });
 
-            opt.value = i;
-            opt.text = this.characterList[i]["name"];
-        }
+            this.guess_select.replaceChildren();
+
+            for (let i=0; i<possible_chr.length; i++) {
+                let line = document.createElement("div");
+                let div_img = document.createElement("div");
+                let image = document.createElement("img");
+                let text = document.createElement("p");
+
+                image.src = "assets/character/" + possible_chr[i]["image"];
+                image.classList.add("guess-img", "center-margin");
+                div_img.appendChild(image);
+                div_img.classList.add("div-img");
+                text.innerHTML = possible_chr[i]["name"];
+                line.append(div_img, text);
+                line.classList.add("left-margin");
+
+                this.guess_select.append(line);
+            }
+        });
 
         console.log(this.characterList);
     }
