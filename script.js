6d7a1a396e2dd8f64df5f1b68de6c8ecd60b7206
Dan3Now <s3perdani@gmail.com>
2025-03-26 22:28:58 +0100

    add event for adding the guessed character

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ class Game {
         this.list = list;
 
         this.characterList = [];
+        this.columnName = [];
+        this.imgDir = "";
 
         this.guess_input = document.getElementById("guess_input");
         this.guess_select = document.getElementById("guess_select");
@@ -17,14 +19,16 @@ class Game {
         const response = await fetch(this.list);
         var json_data = await response.json();
         this.characterList = json_data["fr"];
+        this.imgDir = "assets/character/";
         
         //complete guess header
         for (let i=0; i<Object.keys(this.characterList[0]).length; i++) {
-            let columnName = Object.keys(this.characterList[0])[i];
+            let column = Object.keys(this.characterList[0])[i];
             let newDiv = document.createElement("div");
-            let newContent = document.createTextNode(columnName);
+            let newContent = document.createTextNode(column);
             newDiv.appendChild(newContent);
             this.guess_header.append(newDiv);
+            this.columnName.push(column)
         }
 
         //adapt characterList
@@ -34,6 +38,7 @@ class Game {
         });
 
         //add event
+        //event for guess input
         this.guess_input.addEventListener("change", e => {
             let possible_chr = this.characterList.filter(chr => {
                 return chr["name"].toLowerCase().includes(e.target.value);
@@ -42,20 +47,43 @@ class Game {
             this.guess_select.replaceChildren();
 
             for (let i=0; i<possible_chr.length; i++) {
-                let line = document.createElement("div");
+                let ppr = document.createElement("div");
                 let div_img = document.createElement("div");
                 let image = document.createElement("img");
                 let text = document.createElement("p");
 
-                image.src = "assets/character/" + possible_chr[i]["image"];
+                image.src = this.imgDir + possible_chr[i]["image"];
                 image.classList.add("guess-img", "center-margin");
                 div_img.appendChild(image);
                 div_img.classList.add("div-img");
                 text.innerHTML = possible_chr[i]["name"];
-                line.append(div_img, text);
-                line.classList.add("left-margin");
+                ppr.append(div_img, text);
+                ppr.classList.add("left-margin");
 
-                this.guess_select.append(line);
+                this.guess_select.append(ppr);
+
+                ppr.addEventListener("click", e => {
+                    let line = document.createElement("div");
+                    line.classList.add("line")
+
+                    for (let j=0; j<this.columnName.length; j++) {
+                        if (this.columnName[j] == "image") {
+                            var elt = document.createElement("img");
+                            elt.src = this.imgDir + possible_chr[i]["image"];
+                        } else {
+                            var elt = document.createElement("p");
+                            elt.innerHTML = possible_chr[i][this.columnName[j]];
+                            //console.log(possible_chr[i]["image"]);
+                        }
+                        
+                        let div = document.createElement("div");
+                        div.appendChild(elt);
+                        line.appendChild(div);
+                    }
+                    
+                    console.log(line);
+                    this.guess_lines.appendChild(line);
+                })
             }
         });
 
